fd3d91ec1771aef09c6c59bd4f23d0ac12e577da
Paddy Downey <G00392495@atu.ie>
2025-01-06 23:27:42 +0000

    added h2 testing with a table

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ROOM (
+                      ID BIGINT PRIMARY KEY,
+                      NAME VARCHAR(255),
+                      DATE_AVAILABLE DATE,
+                      IS_BOOKED BOOLEAN
+);
