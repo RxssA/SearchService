9fe7f73639a1de54c413ab4270912f24847d4323
Paddy Downey <G00392495@atu.ie>
2025-01-08 23:25:55 +0000

    finished

--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE ROOM (
-                      ID BIGINT PRIMARY KEY,
-                      NAME VARCHAR(255),
-                      DATE_AVAILABLE DATE,
-                      IS_BOOKED BOOLEAN
-);
